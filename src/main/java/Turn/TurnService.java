4ee01fd6c510d7e5249f1e7e375a5f731e02f141
breadman98 <swo98@naver.com>
2023-05-04 13:03:06 +0900

    TurnService Logic Updated

--- src/main/java/Turn/TurnService.java
+++ src/main/java/Turn/TurnService.java
@@ -21,12 +21,24 @@ public class TurnService {
     public void setCoordinate(Coordinate coordinate) {
         if (currentPlayer == null) {
             currentPlayer = player1;
-        } else {
+        } else { // p1이면 p2로, p2면 p1로 턴 전환
             currentPlayer = currentPlayer == player1 ? player2 : player1;
         }
-        previousCoordinate = coordinate;
-        currentPlayer.setMyTurn(true);
+        if (isValidObstacle(coordinate)) { // 좌표에 놓을 수 있는 경우에만 좌표 변경하고 턴 전환
+            previousCoordinate = coordinate;
+            currentPlayer.setMyTurn(true);
+        } else {
+            throw new RuntimeException("Invalid coordinate");
+        }
     }
+    
+    // temp method
+    public boolean isValidObstacle(Coordinate coordinate) {
+    	return true;
+    }
+    // 임시메소드. 
+    // 리퀘스트로 받은 좌표값으로 알고리즘을 수행하고, 그 알고리즘을 여기서 어떻게
+    // 불러와서 써야하는지 고민중
     
     public Player getPlayerById(String playerId) {
         if (player1.getId().equals(playerId)) {
